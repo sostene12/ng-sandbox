b26c5c9b0029b182b672d29c2ab4ad9bb44a11ec
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2023-10-18 11:09:44 +0200

    ft(data-bind): feature data binding
    - implemented data binding on the shopping list and on the recipes on displaying the single recipe and passing the data between components and on the shopping list to create an ingredient
    
    [WIP shopping-recipes ]

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Input, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
+  @Input() recipe:Recipe = new Recipe('','','');
+  @Output() recipeSelected:EventEmitter<Recipe> = new EventEmitter<Recipe>()
 
+  isEmpty(recipe: Recipe): boolean {
+    return recipe.name.trim() !== '' && recipe.decription.trim() !== '' && recipe.imagePath.trim() !== '';
+  }
+
+  onSelected(){
+    this.recipeSelected.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is simply a test', 'https://realfood.tesco.com/media/images/1400x919-HuntersChicken-1cddb648-ead8-4325-8e60-f80d1ca4ede3-0-1400x919.jpg'),
-    new Recipe('A test recipe', 'this is simply a test', 'https://realfood.tesco.com/media/images/1400x919-HuntersChicken-1cddb648-ead8-4325-8e60-f80d1ca4ede3-0-1400x919.jpg')
-  ]
+    new Recipe('Another test recipe', 'this is simply another  test', 'https://realfood.tesco.com/media/images/1400x919-HuntersChicken-1cddb648-ead8-4325-8e60-f80d1ca4ede3-0-1400x919.jpg')
+  ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
